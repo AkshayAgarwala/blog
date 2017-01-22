287eda4bf3f15f0406e6a726c0e84f7e171bd795
Akshay Agarwala <akshay.agarwala2010@gmail.com>
2017-01-21 22:49:59 -0500

    Added Blog Pages

---
+++ index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var app = express();
+
+var pg = require('pg');
+var bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost:5432/blog';
+
+app.get('/blog', function(req, res){
+  pg.connect(connectionString, function(err, client, done){
+    client.query(`select * from posts`, function(err, result){
+      res.render('blog', {posts: result.rows});
+      done();
+      pg.end();
+    })
+  })
+})
+
+app.post('/blog', function(req, res){
+  pg.connect(connectionString, function(err, client, done){
+    client.query(`insert into posts (title, body) values ('${req.body.title}', '${req.body.body}')`, function(err, result){
+      res.redirect('/blog');
+      done();
+      pg.end();
+    })
+  })
+})
+
+app.get('/blog/:post_id', function(req, res){
+  pg.connect(connectionString, function(err, client, done){
+    client.query(`select * from posts where id = '${req.params.post_id}'`, function(err, result){
+      res.render('post', {post: result.rows[0]});
+      done();
+      pg.end();
+    })
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+  console.log('Listening on port 3000....');
+})
